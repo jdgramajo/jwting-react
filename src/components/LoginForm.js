15ce67bc48f1993e3abf5da356f9ae7eb0226bd5
JD <jdgramajo@gmail.com>
2021-10-03 19:24:43 -0600

    React form handling, work in progress.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -10,10 +10,11 @@ const LoginForm = () => {
     event.preventDefault();
     const userName = document.getElementById("user-input").value;
     const password = document.getElementById("password-input").value;
+
     setUserName(userName);
     setPassword(password);
     console.log(userName, password);
-  }
+  };
   // - Validate form the React way.
   // - Insert info. and pwd. into application state.
 
@@ -21,42 +22,49 @@ const LoginForm = () => {
     <div className="container col-12 col-sm-10 col-md-9 col-lg-8 col-xl-6 col-xxl-4">
       <div>
         <div className="card p-5">
-          <form id="login-form" noValidate={true} onSubmit={changeStore} >
+          <form id="login-form" noValidate={true}>
             <div className="d-flex flex-column">
               <div className="d-flex justify-content-center h3 mb-3">Login</div>
               <div className="my-2 form-floating">
-                <input id="user-input"
-                       type="text"
-                       placeholder="Usuario"
-                       className="form-control"
-                       required={true}
-                       defaultValue={userName} />
+                <input
+                  id="user-input"
+                  type="text"
+                  placeholder="Usuario"
+                  className="form-control"
+                  required={true}
+                  defaultValue={userName}
+                />
                 <label htmlFor="user-input">Usuario</label>
-                <div className="invalid-feedback">
-                  Usuario es requerido.
-                </div>
+                <div className="invalid-feedback">Usuario es requerido.</div>
               </div>
               <div className="mb-2 form-floating">
-                <input id="password-input"
-                       type="password"
-                       placeholder="Contraseña"
-                       className="form-control"
-                       required={true}
-                       defaultValue={password} />
+                <input
+                  id="password-input"
+                  type="password"
+                  placeholder="Contraseña"
+                  className="form-control"
+                  required={true}
+                  defaultValue={password}
+                />
                 <label htmlFor="user-input">Contraseña</label>
-                <div className="invalid-feedback">
-                  Contraseña es requerida.
-                </div>
+                <div className="invalid-feedback">Contraseña es requerida.</div>
               </div>
               <div className="d-flex justify-content-center mt-3">
-                <button id="submit-button" type="submit" className="btn btn-lg btn-outline-success w-50">Ingresar</button>
+                <button
+                  id="submit-button"
+                  type="submit"
+                  className="btn btn-lg btn-outline-success w-50"
+                  onClick={changeStore}
+                >
+                  Ingresar
+                </button>
               </div>
             </div>
           </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoginForm;
