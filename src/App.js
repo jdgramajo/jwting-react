8d4a126091fe05cf2c5f809fc0968b192d74bfe7
JD <jdgramajo@gmail.com>
2021-10-09 13:53:42 -0600

    Starts using React Router.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import LoginForm from './components/LoginForm'
+import LoginForm from "./components/LoginForm";
 
 function App() {
   const { userInfo } = useSelector((store) => store);
-  const loginForm = userInfo ? <LoginForm /> : <></>;
 
   const style = { marginTop: "100px" };
 
   return (
-    <div className="container" style={style}>
-      {loginForm}
-    </div>
+    <Router>
+      <div className="container" style={style}>
+        <Switch>
+          <Route exact={true} path="/">
+            <LoginForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
