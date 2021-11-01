491d01ed9d50bb84bee01c8884911294485a718f
JD <jdgramajo@gmail.com>
2021-11-01 15:58:57 -0600

    Fixes fetch usage, cleans up code, work in progress.

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,35 +1,33 @@
 import React from "react";
-import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-import { changeUserRoles, useGetMyRolesQuery } from "../store";
+import { useGetMyRolesQuery } from "../store";
 
 const UserInfo = () => {
   // TODO:
   // - Research why upon a refresh, redirection to main happens.
-  const { userName, userRoles } = useSelector((store) => store);
-  const dispatch = useDispatch();
+  // - The above also keeps the roles data for the next login, which is not desired.
+  const { userName } = useSelector((store) => store);
   const history = useHistory();
 
   if (!userName) history.push("/");
+
   const { data } = useGetMyRolesQuery();
-
-  useEffect(() => {
-    dispatch(changeUserRoles(data?.roles ?? []));
-  }, [data, dispatch]);
-
-  const roles = userRoles.map((role) => <li key={role}>{role}</li>);
 
   return (
     <div>
-      <p>
+      <div>
         <div className="text-body display-3">Hi {userName}!</div>
-      </p>
-      <p>
+      </div>
+      <div>
         <div className="text-body display-5">Your roles are:</div>
-        <ul className="text-body display-6">{roles}</ul>
-      </p>
+        <ul className="text-body display-6">
+          {data?.roles?.map((role) => (
+            <li key={role}>{role}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
